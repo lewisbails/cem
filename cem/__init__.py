6f68060e6a449516677eb8a5ccb2aaa04585a0fd
Lewis Bails <lewis.bails@ripjar.com>
2023-09-17 09:36:00 +0100

    Use poetry

--- cem/__init__.py
+++ cem/__init__.py
@@ -1,5 +1,6 @@
 __author__ = """Lewis Bails"""
-__email__ = 'lewis.bails@gmail.com'
-__version__ = '0.1.5'
+__email__ = "lewis.bails@gmail.com"
+__version__ = "0.1.5"
 from .cem import CEM
+
 __all__ = ["CEM"]
--- cem/cem.py
+++ cem/cem.py
@@ -4,7 +4,7 @@ from .imbalance import _imbalance, _generate_imbalance_schema, _coarsen
 
 
 class CEM:
-    '''
+    """
     The CEM class allows users to experiment with different coarsening schemas on a single DataFrame.
     The "imbalance" and "match" methods return the multivariate imbalance (pre or post matching) and individual observation weights post-matching, respectively.
 
@@ -37,16 +37,18 @@ class CEM:
         Independent coarsening schema used to calculate multivariate imbalance (pre or post matching)
     measure : str
         Multivariate imbalance measure
-    '''
+    """
 
-    def __init__(self,
-                 data: pd.DataFrame,
-                 treatment: str,
-                 outcome: str,
-                 H: int = None,
-                 measure: str = 'l1',
-                 lower_H: int = 1,
-                 upper_H: int = 10):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        treatment: str,
+        outcome: str,
+        H: int = None,
+        measure: str = "l1",
+        lower_H: int = 1,
+        upper_H: int = 10,
+    ):
         self.data = data.copy()
         self.treatment = treatment
         self.outcome = outcome
@@ -58,7 +60,7 @@ class CEM:
             self.H, self.imbalance_schema = self._find_H_and_schema(lower_H, upper_H)
 
     def imbalance(self, coarsening: dict = None) -> float:
-        '''Calculate the multivariate imbalance remaining after matching the data using some coarsening schema
+        """Calculate the multivariate imbalance remaining after matching the data using some coarsening schema
 
         Parameters
         ----------
@@ -72,13 +74,13 @@ class CEM:
         -------
         float
             The residual imbalance
-        '''
+        """
         weights = self.match(coarsening) if coarsening else None
         df = _coarsen(self.data, self.imbalance_schema)
         return _imbalance(df.drop(columns=self.outcome), self.treatment, self.measure, weights)
 
     def match(self, coarsening: dict = None) -> pd.Series:
-        ''' Perform coarsened exact matching using some coarsening schema and return the weights for each observation
+        """Perform coarsened exact matching using some coarsening schema and return the weights for each observation
 
         Parameters
         ----------
@@ -92,13 +94,13 @@ class CEM:
         -------
         pandas.Series
             The weight to use for each observation of the provided data given the coarsening schema provided
-        '''
+        """
         if coarsening is None:
             coarsening = self.imbalance_schema
         return _match(self.data.drop(columns=self.outcome), self.treatment, coarsening)
 
     def _find_H_and_schema(self, lower: int, upper: int):
-        print('Calculating H, this may take a few minutes.')
+        print("Calculating H, this may take a few minutes.")
         n_bins = range(lower, upper)
         imb = {}
         df = self.data.drop(columns=self.outcome)
@@ -108,15 +110,15 @@ class CEM:
             imb_h = _imbalance(df_coarse, self.treatment, self.measure)
             if isinstance(imb_h, pd.DataFrame):
                 # use the mean imbalance considering all treatment level pairs
-                imb_h = imb_h['imbalance'].mean()
+                imb_h = imb_h["imbalance"].mean()
             imb[H] = [imb_h, imbalance_schema]
-        imb = pd.DataFrame.from_dict(imb, orient='index', columns=['imbalance', 'schema'])
-        H = (imb['imbalance'].sort_values(ascending=False) <= imb['imbalance'].quantile(.5)).idxmax()
-        return H, imb.loc[H, 'schema']
+        imb = pd.DataFrame.from_dict(imb, orient="index", columns=["imbalance", "schema"])
+        H = (imb["imbalance"].sort_values(ascending=False) <= imb["imbalance"].quantile(0.5)).idxmax()
+        return H, imb.loc[H, "schema"]
 
 
 def _match(data: pd.DataFrame, treatment: str, coarsening: dict) -> pd.Series:
-    '''Return weights for data given a coursening schema
+    """Return weights for data given a coursening schema
 
     Parameters
     ----------
@@ -134,7 +136,7 @@ def _match(data: pd.DataFrame, treatment: str, coarsening: dict) -> pd.Series:
     -------
     pandas.Series
         The weight to use for each observation of the provided data given the coarsening provided
-    '''
+    """
     # coarsen based on supplied coarsening schema
     df_coarse = _coarsen(data, coarsening)
 
@@ -143,7 +145,7 @@ def _match(data: pd.DataFrame, treatment: str, coarsening: dict) -> pd.Series:
 
 
 def _weight(data: pd.DataFrame, treatment: str) -> pd.Series:
-    '''Weight observations based on global and local (strata) treatment level populations'''
+    """Weight observations based on global and local (strata) treatment level populations"""
     # only keep stata with examples from each treatment level
     # if the treatment is continuous and was not coarsened, this will almost certainly
     # result in 0 weight for all examples
@@ -156,13 +158,16 @@ def _weight(data: pd.DataFrame, treatment: str) -> pd.Series:
     matched_counts = matched[treatment].value_counts()
     weights = pd.concat([_weight_stratum(stratum[treatment], matched_counts) for _, stratum in matched.groupby(gb)])
     weights = weights.add(prematched_weights, fill_value=0)
-    weights.name = 'weights'
+    weights.name = "weights"
     return weights
 
 
 def _weight_stratum(treatment_levels: pd.Series, M: pd.Series) -> pd.Series:
-    '''Calculate weights for observations in an individual stratum'''
+    """Calculate weights for observations in an individual stratum"""
     ms = treatment_levels.value_counts()  # local counts for levels of the treatment variable
     T = treatment_levels.max()  # use as "under the policy" level
-    stratum_weights = pd.Series([1 if t == T else (M[t] / M[T]) * (ms[T] / ms[t]) for t in treatment_levels], index=treatment_levels.index)
+    stratum_weights = pd.Series(
+        [1 if t == T else (M[t] / M[T]) * (ms[T] / ms[t]) for t in treatment_levels],
+        index=treatment_levels.index,
+    )
     return stratum_weights
